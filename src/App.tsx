567d13a705cd287ea064f7db7f6d00c6cee364e0
Dmitry Rylov <ryliov.work@gmail.com>
2022-07-29 09:59:26 +0200

    layout: created WeeklyResults components
    Also changed App.tsx for future
    to create right side with TodayInfo, TodayFact, ProgressChart components

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import React from "react"
 // components
 import Header from "./components/Header/Header"
 import Sidebar from "./components/Sidebar/Sidebar"
+import WeeklyResults from "./components/WeeklyResults/WeeklyResults"
 import SignUpPage from "./pages/SignUpPage/SignUpPage"
 // styles
 import './App.scss'
@@ -13,8 +14,11 @@ const App: React.FC = () => {
   return (
     <div className="app">
       <Sidebar />
-      <main>
+      <main className="main">
         <Header />
+        <div className="main__content">
+          <WeeklyResults />
+        </div>
         {/* <SignUpPage /> */}
       </main>
     </div>
--- src/components/WeeklyResults/WeeklyResults.tsx
+++ src/components/WeeklyResults/WeeklyResults.tsx
@@ -1,1 +1,40 @@
-export { }
+// react
+import React from "react"
+
+// additional functional
+// components
+// styles
+import './WeeklyResults.scss'
+
+const WeeklyResults: React.FC = () => {
+  return (
+    <div className="weekly-results shadow br10">
+      <h3 className="weekly-results__title">Успіхи за неділю</h3>
+      <ul className="weekly-results__info">
+        <li className="weekly-results__item">
+          <h4 className="weekly-results__info-title">Створено</h4>
+          <div className="weekly-results__circle">
+            113<br />
+            <span>задач</span>
+          </div>
+        </li>
+        <li className="weekly-results__item">
+          <h4 className="weekly-results__info-title">Завершено</h4>
+          <div className="weekly-results__circle">
+            97<br />
+            <span>задач</span>
+          </div>
+        </li>
+        <li className="weekly-results__item">
+          <h4 className="weekly-results__info-title">Видалено</h4>
+          <div className="weekly-results__circle">
+            14<br />
+            <span>задач</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default WeeklyResults
