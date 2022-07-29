5ee3dc15385df4315bb54d0ba042607d0325d49d
Dmitry Rylov <ryliov.work@gmail.com>
2022-07-29 10:03:07 +0200

    add: ProgressChart component

---
+++ src/components/ProgressChart/ProgressChart.tsx
@@ -0,0 +1,13 @@
+// react
+import React from "react"
+
+// additional functional
+// components
+// styles
+import './ProgressChart.scss'
+
+const ProgressChart: React.FC = () => {
+  return null
+}
+
+export default ProgressChart
