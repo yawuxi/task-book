91be42de607ba263bae5d103f7bed799e81bfddf
Dmitry <ryiov.work@gmail.com>
2022-08-18 17:47:01 +0200

    chore: clean up context and actionTypes, read desc
    Removed unnecessary properties, reducer functions

--- src/shared/actionTypes.ts
+++ src/shared/actionTypes.ts
@@ -1,5 +1,4 @@
 export const ACTION_TYPES = {
-  LOAD_DATA_FROM_FIREBASE: 'LOAD_DATA_FROM_FIREBASE',
   theme: {
     SET_THEME: 'SET_THEME',
   },
@@ -8,23 +7,14 @@ export const ACTION_TYPES = {
   },
   sidebar: {
     TOGGLE_BURGER_MENU: 'TOGGLE_BURGER_MENU',
-    ADD_REF_ELEMENT: 'ADD_REF_ELEMENT',
   },
   modals: {
     modalTextWindow: {
       TOGGLE_TEXT_MODAL: 'TOGGLE_MODAL',
-      TEXT_MODAL_ADD: 'MODAL_ADD',
     },
     createTask: {
       TOGGLE_CREATE_TASK: 'TOGGLE_CREATE_TASK',
     }
-  },
-  taskItem: {
-    COMPLETE_TASK: 'COMPLETE_TASK',
-    REMOVE_TASK: 'REMOVE_TASK',
-  },
-  taskItemTemplate: {
-    ADD_TASK_TEMPLATE: 'ADD_TASK_TEMPLATE',
   },
   activePointOffset: {
     CHANGE_POINT_OFFSET: 'CHANGE_POINT_OFFSET',
--- src/shared/context.tsx
+++ src/shared/context.tsx
@@ -4,28 +4,8 @@ import { iInitialState } from "../types/InitialState";
 import { iAction } from "../types/Action";
 
 const initialState: iInitialState = {
-  displayName: '',
   theme: localStorage.getItem('theme'),
-  tasksList: [
-    // {
-    //   task: 'Зробити блінчики',
-    //   category: 'їжа',
-    //   date: '2020-08-01',
-    //   priority: 'Дуже важливо',
-    //   id: nextId(),
-    //   isCompleted: false
-    // },
-  ],
-  taskItemTemplates: [],
-  createTaskPriorities: [],
-  createTaskCategories: [],
   sidebar: {
-    categories: [
-      { title: 'Дім', path: '/' },
-      { title: "Сім'я", path: '/family' },
-      { title: 'Робота', path: '/work' },
-      { title: 'Спорт', path: '/sport' },
-    ],
     burgerMenu: false,
   },
   header: {
@@ -43,7 +23,6 @@ const TaskBookReducer = (state: iInitialState, action: iAction) => {
   const { type, payload } = action
 
   const {
-    LOAD_DATA_FROM_FIREBASE,
     theme: { SET_THEME },
     header: { TOGGLE_MENU },
     sidebar: {
@@ -52,17 +31,13 @@ const TaskBookReducer = (state: iInitialState, action: iAction) => {
     modals: {
       modalTextWindow: {
         TOGGLE_TEXT_MODAL,
-        TEXT_MODAL_ADD,
       },
       createTask: { TOGGLE_CREATE_TASK, },
     },
-    taskItem: { COMPLETE_TASK, REMOVE_TASK },
-    taskItemTemplate: { ADD_TASK_TEMPLATE },
     activePointOffset: { CHANGE_POINT_OFFSET },
   } = ACTION_TYPES
 
   const {
-    tasksList,
     modals,
     sidebar,
   } = state
@@ -70,19 +45,6 @@ const TaskBookReducer = (state: iInitialState, action: iAction) => {
   switch (type) {
     default:
       return state
-    // load data from firebase
-    // case LOAD_DATA_FROM_FIREBASE:
-    //   console.log(payload);
-    //   return { ...state }
-    case LOAD_DATA_FROM_FIREBASE:
-      return {
-        ...state,
-        displayName: payload.displayName,
-        tasksList: [...payload.tasksList],
-        taskItemTemplates: [...payload.taskItemTemplates],
-        createTaskPriorities: [...payload.createTaskPriorities],
-        createTaskCategories: [...payload.createTaskCategories],
-      }
     // theme
     case SET_THEME:
       return { ...state, theme: payload }
@@ -91,35 +53,12 @@ const TaskBookReducer = (state: iInitialState, action: iAction) => {
       return { ...state, modals: { ...modals, modalTextWindow: { isOpen: !modals.modalTextWindow.isOpen } } }
     case TOGGLE_BURGER_MENU:
       return { ...state, sidebar: { ...sidebar, burgerMenu: !sidebar.burgerMenu } }
-    case TEXT_MODAL_ADD:
-      return { ...state, sidebar: { ...sidebar, categories: [...sidebar.categories, { title: payload.title, path: payload.path }] } }
     // header
     case TOGGLE_MENU:
       return { ...state, header: { toggleMenu: !state.header.toggleMenu } }
     // CreateTask
     case TOGGLE_CREATE_TASK:
       return { ...state, modals: { ...modals, createTask: { isOpen: !modals.createTask.isOpen } } }
-    // TaskItem
-    case COMPLETE_TASK:
-      return {
-        ...state,
-        tasksList: tasksList.map(item => {
-          if (item.id !== payload) return item
-
-          return { ...item, isCompleted: !item.isCompleted }
-        })
-      }
-    case REMOVE_TASK:
-      return {
-        ...state,
-        tasksList: tasksList.filter(item => item.id !== payload)
-      }
-    // taskItemTemplate
-    case ADD_TASK_TEMPLATE:
-      return {
-        ...state,
-        taskItemTemplates: [...state.taskItemTemplates, payload]
-      }
     // activePointOffset
     case CHANGE_POINT_OFFSET:
       return { ...state, activePointOffset: payload }
